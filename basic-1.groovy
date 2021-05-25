a2bfd4d001eb49a233f486dbe129ab9feb1db0f1
root <you@example.com>
2021-05-25 07:27:00 +0530

    h

--- basic-1.groovy
+++ basic-1.groovy
@@ -1,16 +1,20 @@
 #!/usr/bin/env groovy
 class GroovyTut {
    static void main(String[] arga){
-     def name = "Rama"
-     def age = "20"
+     def name = "groovy"
       //println("My Name is: " + name[0..2])
       //def age = '''20 25 30'''
       //println(++age)
       //println("age++ = " + (age++));
       //println("++age = " + (++age))
-      println("My name is: " +  name.toUpperCase())
-      println("My name is: " +  name.toLowerCase())
-
+      //println("My name is: " +  name.toUpperCase())
+      //println("My name is: " +  name.toLowerCase())
+      //println("My name is: " +  name.length())
+      println(name.toList())
+      println(name.replaceAll('groovy', 'python'))
+      println("Groovy <=> groovy")
+      printf("A %s string \n", name) //printf
+      
    }
 
 }
