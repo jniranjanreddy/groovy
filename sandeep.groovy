3f65418893be06bd7c3688a4489547e95e68d6c1
root <you@example.com>
2021-05-28 08:08:59 +0530

    j

--- sandeep.groovy
+++ sandeep.groovy
@@ -3,21 +3,3 @@
 new File("/etc/hosts").eachLine { line ->
 //println(line)
 }
-
-def greeting = "Goodbye"
-
-def sayGoodbye = {theName ->
-println("$greeting $theName")}
-
-sayGoodbye("Derek")
-
-Boolean isDevelop = false
-Boolean isProduction = false
-def branchName = "production"
-
-isDevelop = branchName == 'develop'
-isProduction = branchName == 'production'
-
-
-targetEnvCluster = (isProduction) ? 'production' : 'develop'
-println(targetEnvCluster)
